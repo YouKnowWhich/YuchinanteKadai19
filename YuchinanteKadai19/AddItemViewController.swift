a9eed1f15f403cbf2753467f56d3050f5013c49f
yuya-3110 <ysuu45143745@gmail.com>
2024-07-02 01:03:13 +0900

    first commit

---
+++ YuchinanteKadai19/AddItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddItemViewController.swift
+//  YuchinanteKadai19
+//  
+//  Created by Yuchinante on 2024/07/01
+//  
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YuchinanteKadai19/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  YuchinanteKadai19
+//  
+//  Created by Yuchinante on 2024/07/02
+//  
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
